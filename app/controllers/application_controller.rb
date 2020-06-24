f9a1e69195a4b8d0226bc6da8c9541f412b6b555
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-06-24 15:25:34 +0900

    Create a form to submit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   def index
     @posts = Post.all
   end
+
+  def create
+    Post.create(post_params)
+    redirect_to "/"
+  end
+
+  private
+  def post_params
+    params.permit(:content)
+  end
 end
