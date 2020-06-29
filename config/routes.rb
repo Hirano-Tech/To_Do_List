8380fdc15540569765354506e8be609694351485
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-06-29 16:46:34 +0900

    Modify the view file.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'application#index'
     # ルートパス （get '/', to: '[コントローラー名]#[アクション名]'） にリクエストする。
 
+  resources :application, only: [:index, :create]
   post '/', to: 'application#create'
 end
